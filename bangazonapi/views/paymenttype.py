bb0249e2145de5d621626fb970bcbf375b7908cf
Geno McNew <genemcnew3@gmail.com>
2021-02-09 10:25:00 -0600

    added user auth to payment types

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -81,10 +81,10 @@ class Payments(ViewSet):
         """Handle GET requests to payment type resource"""
         payment_types = Payment.objects.all()
 
-        customer_id = self.request.query_params.get('customer', None)
+        customer_id = Customer.objects.get(user=request.auth.user)
 
         if customer_id is not None:
-            payment_types = payment_types.filter(customer__id=customer_id)
+            payment_types = payment_types.filter(customer__id=customer_id.id)
 
         serializer = PaymentSerializer(
             payment_types, many=True, context={'request': request})
