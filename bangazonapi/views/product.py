8ddfb3b4239cf284480ec1d7085079af63b8a1da
Geno McNew <genemcnew3@gmail.com>
2021-02-10 13:34:22 -0600

    Revert "added min_price query string"
    This reverts commit 0cfa9dadfd5aff3c763ef62b0075639a799b5ad4.

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -249,7 +249,6 @@ class Products(ViewSet):
         direction = self.request.query_params.get('direction', None)
         number_sold = self.request.query_params.get('number_sold', None)
         location = self.request.query_params.get('location', None)
-        min_price = self.request.query_params.get('min_price', None)
 
         if order is not None:
             order_filter = order
@@ -273,14 +272,6 @@ class Products(ViewSet):
                 return False
 
             products = filter(sold_filter, products)
-
-        if min_price is not None:
-            def price_filter(product):
-                if product.price >= int(min_price):
-                    return True
-                return False
-
-            products = filter(price_filter, products)
 
         if location is not None:
             products = products.filter(location__contains=location)
