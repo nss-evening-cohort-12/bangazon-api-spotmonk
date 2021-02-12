9f222dade3162eec56275c875ed5b2b539f58949
Geno McNew <genemcnew3@gmail.com>
2021-02-12 09:32:48 -0600

    syntax

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -3,6 +3,6 @@ from .views import expensive_list, Cheap_list, completed_order_list
 
 urlpatterns = [
     path('reports/expensive', expensive_list),
-    path('reports/orders/completed', completed_order_list)
-     path('reports/cheap', Cheap_list)
+    path('reports/orders/completed', completed_order_list),
+    path('reports/cheap', Cheap_list)
 ]
