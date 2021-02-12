945e7a64bcbaa0363bd3067d092481472a165020
Geno McNew <genemcnew3@gmail.com>
2021-02-12 07:27:08 -0600

    removed commit from previous pr

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,4 +1,3 @@
 from .connection import Connection
 from .products.expensiveproducts import expensive_list
-from .products.cheapproducts import Cheap_list
 from .orders.completedorders import completed_order_list
