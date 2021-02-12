73f1ec98921a6b3be733c093fcd28965d8997ffa
Geno McNew <genemcnew3@gmail.com>
2021-02-12 09:13:33 -0600

    removed leftover import from conflict

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import expensive_list, Cheap_list, completed_order_list
+from .views import expensive_list, completed_order_list
 
 urlpatterns = [
     path('reports/expensive', expensive_list),
-    path('reports/cheap', Cheap_list),
     path('reports/orders/completed', completed_order_list)
