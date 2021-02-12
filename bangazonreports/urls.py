a596589adcdc83857052efa412e00a2fd6a92a56
Geno McNew <genemcnew3@gmail.com>
2021-02-12 07:30:12 -0600

    resolved conflict

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import expensive_list, Cheap_list, completed_order_list
 
 urlpatterns = [
     path('reports/expensive', expensive_list),
     path('reports/cheap', Cheap_list),
     path('reports/orders/completed', completed_order_list)
-=======
-from .views import expensive_list
-
-urlpatterns = [
-    path('reports/expensive', expensive_list),
->>>>>>> parent of d86c2c5... added inexpensive products report
-]
