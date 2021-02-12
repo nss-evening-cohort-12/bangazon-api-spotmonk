06835ff780056fee4affd0ab4573359cd3d568f3
Geno McNew <genemcnew3@gmail.com>
2021-02-12 12:59:58 -0600

    added incomplete order report

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import expensive_list, Cheap_list, completed_order_list
+from .views import expensive_list, Cheap_list, completed_order_list, incomplete_order_list
 
 urlpatterns = [
     path('reports/expensive', expensive_list),
     path('reports/orders/completed', completed_order_list),
-    path('reports/cheap', Cheap_list)
+    path('reports/cheap', Cheap_list),
+    path('reports/orders/incomplete', incomplete_order_list)
 ]
