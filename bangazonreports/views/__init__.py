d5f97febcfdc3cae8b6e6b6da89b49fd6fde6e50
Geno McNew <genemcnew3@gmail.com>
2021-02-12 07:23:01 -0600

    added report for completed orders

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .products.expensiveproducts import expensive_list
 from .products.cheapproducts import Cheap_list
+from .orders.completedorders import completed_order_list
