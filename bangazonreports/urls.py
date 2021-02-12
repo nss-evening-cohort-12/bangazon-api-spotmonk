7134b997339075e0c69b0a9bde9292b28aac98a1
Geno McNew <genemcnew3@gmail.com>
2021-02-12 09:27:20 -0600

    reverted revert to merge master

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+<<<<<<< HEAD
 from .views import expensive_list, completed_order_list
+=======
+from .views import expensive_list, Cheap_list, completed_order_list
+>>>>>>> parent of 945e7a6... removed commit from previous pr
 
 urlpatterns = [
     path('reports/expensive', expensive_list),
