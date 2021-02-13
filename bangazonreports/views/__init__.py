b7e4ed7050da11668ac8054a4de2b1bc0f71ef02
Geno McNew <genemcnew3@gmail.com>
2021-02-13 12:00:20 -0600

    can report favorite sellers by user

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -3,3 +3,4 @@ from .products.expensiveproducts import expensive_list
 from .products.cheapproducts import Cheap_list
 from .orders.completedorders import completed_order_list
 from .orders.incompleteorders import incomplete_order_list
+from .favoritesellers.favoritesellers import favorites_list
